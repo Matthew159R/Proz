4e24ad1f4ee076af1d6746981f4e4a31e90f848f
Matthew159R <117731536+Matthew159R@users.noreply.github.com>
2022-11-30 22:06:26 -0300

    Update Media2.py

--- Media2.py
+++ Media2.py
@@ -1,21 +1,3 @@
-i = 1
-maior = 0
-menor = 0
-negativos = 0
-while i != 0:
-    valor = int(input('Digite o número'))
-    if valor == 0:
-        print(f'O maior foi {maior}, o menor foi {menor} e a soma dos negativos foram {negativos}')
-        break
-    elif valor < 0:
-        negativos = valor + negativos
-    elif valor > maior:
-        maior = valor
-        print(f'O maior é {valor}')
-    else:
-        menor = valor
-        print(f'O menor é {menor}')
-
 soma = 0
 i = 1
 qtdNum = int(input('Quantos dados de media?'))
@@ -25,4 +7,4 @@ for i in range(0, qtdNum):
         soma = soma + numbers
         media = soma / qtdNum
         print(media)
-    
+        
